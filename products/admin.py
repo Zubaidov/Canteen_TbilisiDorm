42a7e2a1a3d353c9260adf0e6edd77ba01fbe0a6
Zubaidov <zubaidov.shohruz@gmail.com>
2023-12-18 14:59:22 +0400

    Models and Admin is ready to go with Menu

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Menu
 
+@admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ("barcode", "title", "amount", "price", "created_at")
-
-admin.site.register(Menu, MenuAdmin)
+    list_display = ['title', 'slug', 'author', 'publish', 'status', 'price']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
--- products/models.py
+++ products/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+from django.db.models.query import QuerySet
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class PublishedMenuManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(status=Menu.Status.PUBLISHED)
 
 class Menu(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'DRAFT'
+        PUBLISHED = 'PB', 'PUBLISHED'
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     barcode = models.IntegerField(unique=True)
     amount = models.IntegerField(default=0)
-    title = models.CharField(max_length=255)
     desc = models.TextField(null=True)
     price = models.DecimalField(max_digits=5, decimal_places=1)
-    created_at = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager() # The default manager.
+    published = PublishedMenuManager() # Our custom manager.
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
 
     def __str__(self):
         return f"{self.title}"
